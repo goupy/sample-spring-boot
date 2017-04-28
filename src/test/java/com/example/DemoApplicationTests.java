95a7acebbaa3bac312e9239d02e3c59afa0b5478
Formation <formation@Ubuntu-16.04.2-VBox>
2017-04-28 14:58:17 +0200

    Ajout du test 3 avec Ignore

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Ignore; 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +17,11 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads2() {
 		assert(1==2);
+	}
+	@Ignore
+	@Test
+	public void testDivide() {
+		fail(0==0);
 	}
 
 }
